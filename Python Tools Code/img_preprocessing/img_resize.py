52942bda1c68f714ad26c1bceb39363eb4c73585
Pi Sun Woo <ppssww1202@naver.com>
2023-07-31 16:17:32 +0900

    Enhance Image Preprocessing Code

--- Python Tools Code/img_preprocessing/img_resize.py
+++ Python Tools Code/img_preprocessing/img_resize.py
@@ -1,11 +1,14 @@
 import cv2
 import os
 
-image_path = './KneeXray/test/4/'
-image_path2 = './KneeXray/test/4_he/'
-img_list = os.listdir(image_path)
+for la in range(0, 5):
+    image_path = f'/Users/pisunwoo/Downloads/KL Grade Datasets/HH_Perfection/HH_1/{la}/'
+    image_path2 = f'/Users/pisunwoo/Downloads/KL Grade Datasets/HH_Perfection/HH_1/resize/{la}/'
+    img_list = os.listdir(image_path)
+    png_files = [file for file in img_list if file.endswith(".png")]
+    
 
-for i in img_list:
-    img = cv2.imread('{}{}'.format(image_path, i), cv2.IMREAD_GRAYSCALE)
-    img2 = cv2.resize(img, dsize=(300, 300), interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite('{}{}'.format(image_path2, i), img2)
+    for i in png_files:
+        img = cv2.imread('{}{}'.format(image_path, i), cv2.IMREAD_GRAYSCALE)
+        img2 = cv2.resize(img, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite('{}{}'.format(image_path2, i), img2)
