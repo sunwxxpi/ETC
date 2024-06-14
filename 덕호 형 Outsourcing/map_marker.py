00f68b016c3df3a7a8b2df40d1444c1d0218e766
Sunwoo Pi <ppssww1202@naver.com>
2024-06-15 04:36:15 +0900

    feat: :sparkles: Code complete

--- 덕호 형 Outsourcing/map_marker.py
+++ 덕호 형 Outsourcing/map_marker.py
@@ -25,22 +25,17 @@ def get_geocode(address, api_keys):
     if response.status_code == 200:
         data = response.json()
         
-        if 'addresses' in data:
-            if data['addresses']:
-                return data['addresses'][0]['y'], data['addresses'][0]['x']
-            else:
-                print(f"No addresses found for: {address}")
-                return None, None
+        if 'addresses' in data and data['addresses']:
+            return data['addresses'][0]['y'], data['addresses'][0]['x']
         else:
-            print(f"Unexpected data format for: {address}")
-            print(data)
+            print(f"No addresses found for: {address}")
             return None, None
     else:
         print(f"Request failed for: {address}, Status code: {response.status_code}")
         return None, None
-    
+
 # Load API keys from the JSON file
-with open('naver_maps_api_keys.json') as f:
+with open('./naver_maps_api_keys.json') as f:
     api_keys_json = json.load(f)
 
 # File path to the CSV file containing factory addresses
@@ -50,51 +45,64 @@ df = pd.read_csv(file_path, low_memory=False)
 # Extract the addresses from the DataFrame
 addresses = df['공장대표주소(지번)']
 
-# Dictionary to store coordinates and corresponding row indices
-address_coords = {}
-for i, address in enumerate(tqdm(addresses, desc='Processing', unit=' Addresses'), start=1):
+# List to store coordinates and their corresponding row indices
+address_coords = []
+for i, address in enumerate(tqdm(addresses, desc='Processing', unit='Address'), start=1):
     lat, lon = get_geocode(address, api_keys=api_keys_json)
     
     if lat and lon:
-        key = (float(lat), float(lon))
+        address_coords.append((float(lat), float(lon), i))
 
-        if key not in address_coords:
-            address_coords[key] = []
-
-        address_coords[key].append(i)
+# Sort the coordinates from top to bottom, then left to right
+sorted_coords = sorted(address_coords, key=lambda x: (-x[0], x[1]))
 
 numbering = []
+numbering_map = {}
 
-# Create a Folium map centered around the last obtained coordinates
-map_ = folium.Map(location=[lat, lon], 
-                  zoom_start=15, 
-                  tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', 
-                  attr='ESRI')
+# Create a Folium map centered around the first obtained coordinates
+map_ = folium.Map(location=[sorted_coords[0][0], sorted_coords[0][1]], 
+                  zoom_start=15,
+                  min_zoom=7,
+                  max_zoom=20,
+                #   tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}' # Default
+                #   tiles='http://mt0.google.com/vt/lyrs=s&hl=ko&x={x}&y={y}&z={z}', # Satellite
+                #   tiles='http://mt0.google.com/vt/lyrs=y&hl=ko&x={x}&y={y}&z={z}', # Hybrid
+                #   tiles='https://map.pstatic.net/nrs/api/v1/raster/satellite/{z}/{x}/{y}.jpg?version=5.02', # Satelite
+                  tiles='https://map.pstatic.net/nrb/styles/satellite/1717723233/{z}/{x}/{y}.png?mt=bg.ol.ts.lko', # Hybrid
+                  attr='(NAVER Map) Made by Sunwoo Pi',
+                  font_size='1.5rem')
 
-# Add markers to the map for each unique set of coordinates
-for key, labels in address_coords.items():
-    lat, lon = key
-    label = ", ".join(map(str, labels))
-    address_list = [addresses.iloc[idx - 1] for idx in labels]
-    popup_text = "<br>".join([f"번호: {lbl}, 주소: {addr}" for lbl, addr in zip(labels, address_list)])
+# Dictionary to keep track of which coordinates have been assigned which numbers
+coord_number_map = {}
+for idx, (lat, lon, original_index) in enumerate(sorted_coords, start=1):
+    key = (lat, lon)
+    
+    if key not in coord_number_map:
+        coord_number_map[key] = []
+        
+    coord_number_map[key].append(idx)
+    numbering.append((original_index, idx))
+    numbering_map[original_index - 1] = idx
+
+# Add markers to the map for each unique set of coordinates with combined numbering
+for (lat, lon), num_list in coord_number_map.items():
+    label = "[" + ", ".join(map(str, num_list)) + "]"
+    popup_text = "<br>".join([f"{num} : {addresses.iloc[sorted_coords[num-1][2] - 1]}" for num in num_list])
     
     folium.Marker(
         [lat, lon],
-        popup=folium.Popup(popup_text, max_width=300),
-        icon=folium.DivIcon(html=f'<div style="font-size: 14pt; color: yellow">{label}</div>')
+        popup=folium.Popup(popup_text, max_width=400),
+        icon=folium.DivIcon(html=f'<div style="font-size: 14pt; color: yellow; width: 100px; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">{label}</div>')
     ).add_to(map_)
-    
-    for idx in labels:
-        numbering.append((idx, addresses.iloc[idx - 1]))
 
-# Create a DataFrame with numbering information
-numbering_df = pd.DataFrame(numbering, columns=['Number', 'Address']).sort_values(by='Number')
 # Add numbering information to the original DataFrame
-df['넘버링'] = numbering_df['Number'].values
+df['넘버링'] = df.index.map(numbering_map)
+# Sort the DataFrame by numbering
+df_sorted = df.sort_values(by='넘버링')
+# Save the sorted DataFrame to a new CSV file
+df_sorted.to_csv('./factory_with_numbering_sorted.csv', index=False, encoding='utf-8-sig')
 
-# Save the updated DataFrame to a new CSV file
-df.to_csv('./factory_with_numbering.csv', index=False, encoding='utf-8-sig')
 # Save the Folium map to an HTML file
-map_.save('./factories_map.html')
+map_.save('./factory_map.html')
 
 print("코드 실행 완료.")
